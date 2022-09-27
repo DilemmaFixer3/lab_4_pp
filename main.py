95ea99fb45ae10a3772731634fcad426b6c5d2b2
DilemmaFixer3 <miroslava.babiychuk@gmail.com>
2022-09-28 01:18:05 +0300

    4

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from flask import Flask
 
 app = Flask('__name__')
 
-@app.route('/api/v1/hello-world-1/')
+@app.route('/')
+def index():
+    return " "
+
+@app.route('/api/v1/hello-world-1')
 def hello_world():
     return "<h1>Hello World 1</h1>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
