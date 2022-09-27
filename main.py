c752040eea9fa49d7707373d2df7830605112091
DilemmaFixer3 <miroslava.babiychuk@gmail.com>
2022-09-28 00:22:34 +0300

    2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask('__name__')
 
 @app.route('/api/v1/hello-world-1')
 def hello_world():
-    return "Hello World 1"
+    return "<h1>Hello World 1</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
