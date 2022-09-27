f4d172bbc82257b14fbde272c61859e62169d414
DilemmaFixer3 <miroslava.babiychuk@gmail.com>
2022-09-28 01:07:35 +0300

    3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask('__name__')
 
-@app.route('/api/v1/hello-world-1')
+@app.route('/api/v1/hello-world-1/')
 def hello_world():
     return "<h1>Hello World 1</h1>"
 
